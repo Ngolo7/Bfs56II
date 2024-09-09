2a5ac5bacb35c6dc1733bdfd1ab92d4ab1921394
Practice <gurungriwaaz7@gmail.com>
2024-09-09 13:35:20 -0400

    multiplication of number

---
+++ src/AssignmentThree/MultiplicationOfNumber.java
@@ -0,0 +1,18 @@
+package AssignmentThree;
+
+import static AssignmentThree.Utilities.utils.InputNumber;
+public class MultiplicationOfNumber {
+
+    public static void main(String[] args) {
+
+        int num = InputNumber();
+        MultiplicationOfNumber(num);
+    }
+    public static void MultiplicationOfNumber(int num) {
+        System.out.println("Multiplication Table of " + num + ":");
+        for (int i = 1; i <= 10; i++) {
+            int result = num * i;
+            System.out.println(num + " x " + i + " = " + result);
+        }
+    }
+}
